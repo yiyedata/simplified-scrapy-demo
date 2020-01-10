534ec486bf79f3330d352bb905a7027de6cbe842
yiyedata <49472092+yiyedata@users.noreply.github.com>
2020-01-10 10:57:49 +0800

    Update start.py

--- start.py
+++ start.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python
 #coding=utf-8
 from simplified_scrapy.simplified_main import SimplifiedMain
-
+# If there is something wrong after running, please update the library.
 SimplifiedMain.startThread()
